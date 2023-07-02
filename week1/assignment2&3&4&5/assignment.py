3c5d3a84f924491ee49baa969f847324f7603871
İsmail Kattan <ismail.kattan@ogr.sakarya.edu.tr>
2023-07-02 14:02:24 +0300

    introducing, comments, datatype & variables

---
+++ week1/assignment2&3&4&5/assignment.py
@@ -0,0 +1,16 @@
+#assignment 2: 
+myName = "ismail"
+myAge = "21"
+myCountry = "Turkey"
+#assignment 3: 
+
+print ("Name: " + myName + "\nAge :" + myAge +"\nCountry: " + myCountry)
+
+#assignment 4:
+
+print("Hello, My Name Is " + myName + " And I am " + myAge + " Years Old and I Live in" + myCountry) 
+
+#assignment 5: 
+type(myName)
+type(myAge)
+type(myCountry)
