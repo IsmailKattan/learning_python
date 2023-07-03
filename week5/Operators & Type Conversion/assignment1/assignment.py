7361c5f8a4323790bf9b8efa34435a9256e71b55
İsmail Kattan <ismail.kattan@ogr.sakarya.edu.tr>
2023-07-03 14:03:37 +0300

    Operators & Type Conversion

---
+++ week5/Operators & Type Conversion/assignment1/assignment.py
@@ -0,0 +1,20 @@
+# Needed Output
+
+print(bool(1))
+print(bool(" "))
+print(bool(0.6))
+print(bool(1+2j))
+
+print(bool(0))
+print(bool(""))
+print(bool(0+0j))
+print(bool(()))
+
+# True
+# True
+# True
+# True
+# False
+# False
+# False
+# False
---
+++ week5/Operators & Type Conversion/assignment4/assignment.py
@@ -0,0 +1,18 @@
+num_one = 10
+num_two = 20
+
+result = num_one + num_two
+print(result)
+print(result**3)
+print(result**3%26000)
+print(result**3%26000/5)
+print(type(result**3%26000/5))
+print(type(str(result**3%26000/5)))
+
+# Needed Output
+
+# 30
+# 27000
+# 1000
+# 200.0
+# <class 'str'>
